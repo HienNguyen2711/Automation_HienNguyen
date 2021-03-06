5451625765e5a55b19af68fb52ccc0f14260e77e
unknown <quanghien2711@gmail.com>
2021-03-06 18:48:08 +0700

    Update Exercise_XPath_Topic04/05

--- selenium/webdriver/Topic_06_Xpath_Css_Part_2.java
+++ selenium/webdriver/Topic_06_Xpath_Css_Part_2.java
@@ -8,16 +8,21 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Topic_06_Xpath_Css_Part_2 {
     WebDriver driver;
+    Integer random = new Random().nextInt();
+    String random_email = "HienNguyen" + random + "@gmail.com";
+    String password = "123456";
 
     @BeforeClass
     public void beforeClass() {
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
+
 
     }
 
@@ -70,11 +75,39 @@ public class Topic_06_Xpath_Css_Part_2 {
     }
 
     @Test
-    public void TC_05_LinkText() {
-        }
+    public void Login_5_Create_Acc_Success() {
+        driver.get("http://live.demoguru99.com/");
+        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+        driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+        driver.findElement(By.id("firstname")).sendKeys("Hien");
+        driver.findElement(By.id("middlename")).sendKeys("Quang");
+        driver.findElement(By.id("lastname")).sendKeys("Nguyen");
+        driver.findElement(By.id("email_address")).sendKeys(random_email);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("confirmation")).sendKeys(password);
+        driver.findElement(By.xpath("//button[@title='Register' and @type='submit']")).click();
+        Assert.assertEquals(driver.findElement(By.xpath("//li[@class='success-msg']//span")).getText(),"Thank you for registering with Main Website Store.");
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='page-title']//h1")).getText(),"MY DASHBOARD");
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='welcome-msg']//p[@class='hello']")).getText().matches("Hello, Hien Quang Nguyen!"));
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='box-content']//p[contains(text(),'Hien Quang Nguyen')]")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='box-account box-info']//p[contains(.,'"+random_email+"')]")).isDisplayed());
+        driver.findElement((By.xpath("//a[contains(@class,'skip-account')]"))).click();
+        driver.findElement(By.xpath("//div[@id='header-account']//li[contains(@class,'last')]/a[@title='Log Out']")).click();
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='header-language-container']//p[contains(text(),'Default welcome msg!')]")).isDisplayed());
+    }
 
     @Test
-    public void TC_06_PartialLinkText() {
+    public void Login_6_Login_with_valid_acc() {
+        driver.get("http://live.demoguru99.com/");
+        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+        driver.findElement(By.id("email")).sendKeys(random_email);
+        driver.findElement(By.id("pass")).sendKeys(password);
+        driver.findElement(By.xpath("//button[@id='send2'and @title='Login']")).click();
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='page-title']//h1")).getText(),"MY DASHBOARD");
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='welcome-msg']//p[@class='hello']")).getText().matches("Hello, Hien Quang Nguyen!"));
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='box-content']//p[contains(text(),'Hien Quang Nguyen')]")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='box-account box-info']//p[contains(.,'"+random_email+"')]")).isDisplayed());
+
     }
 
     @Test
